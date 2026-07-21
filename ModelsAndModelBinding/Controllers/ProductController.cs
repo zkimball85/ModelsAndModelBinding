8a2cd8bd313abd48831df666aa020da7ef41e6a9
Zac Kimball <z.kimball85@gmail.com>
2026-07-20 19:32:05 -0700

    Add ProductController with Index and Create actions
    Added ProductController in ModelsAndModelBinding.Controllers namespace.

---
+++ ModelsAndModelBinding/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelsAndModelBinding.Controllers;
+
+public class ProductController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+}
+
