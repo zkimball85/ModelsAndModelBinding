2e60a44cc236a19c176641a8650cfb04cd74801e
zacharykimball <zacharykimball@ZACHARYKIMB6482>
2026-07-20 17:07:51 -0700

    Add project files.

---
+++ ModelsAndModelBinding/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModelsAndModelBinding.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
